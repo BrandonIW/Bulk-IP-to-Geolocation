712e8be920652f2e0fddbe2081a0bdf9c74ce2d7
BrandonW <77559638+BrandonIW@users.noreply.github.com>
2023-06-20 18:36:11 -0700

    Update main.py

--- Moderate - Bulk IP to Geolocation/bin/main.py
+++ Moderate - Bulk IP to Geolocation/bin/main.py
@@ -2,16 +2,23 @@ from requests import get
 from time import sleep
 
 def main():
-    """ Open List of IPs and read each line by line """
+    """ Open List of IPs and read each line by line. Sleeps to not get blocked by API limitations """
+    count = 0
+    
     with open("..\\output\\outputfile.csv", 'w') as outfile:
         outfile.write("IP Address,Version,Country,Region,City,Country Capital,"
                       "Latitude,Longitude,Timezone,UTC-Offset,Country Calling Code,ASN,ORG\n")
+        
         with open("..\\input\\IPlist.txt", 'r') as infile:
             for IP in infile:
                 IP = IP.rstrip("\n")
                 response = get_json(IP)
-                sleep(1)
-
+                count += 1; sleep(1)
+                
+                if count == 40:
+                    sleep(70)
+                    count = 0
+                    
                 if 'error' in response:
                     print(f"There was an error with IP: {IP} Skipping. Error: {response}")
                     continue
